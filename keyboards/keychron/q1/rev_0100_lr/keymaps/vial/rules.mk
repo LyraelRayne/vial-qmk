e0a7c8292e41d2fd10ff3e58e0111bea496ca790
Lyrael Rayne <darkangael@gmail.com>
2022-08-04 23:42:27 +1000

    lr q1 from old keyboard

---
+++ keyboards/keychron/q1/rev_0100_lr/keymaps/vial/rules.mk
@@ -0,0 +1,13 @@
+# Q1 Defaults + VIAL
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+MOUSEKEY_ENABLE = no # 1882 bytes
+
+# Use Less EEPROM
+LTO_ENABLE = yes # Optimise stuff
+# QMK_SETTINGS = no # 2418 bytes.
+# COMBO_ENABLE = no # 2666 bytes
+# EXTRAKEY_ENABLE = no # 366 bytes... yikes
+
+# RGB Control :D
+VIALRGB_ENABLE = yes # 856 bytes

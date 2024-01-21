5b422d18e0300c67251ea0ae20402dcdf4bbd231
Lyrael Rayne <darkangael@gmail.com>
2024-01-21 19:10:19 +1100

    add mods display

--- keyboards/splitkb/elora/keymaps/vial-lr/oled.c
+++ keyboards/splitkb/elora/keymaps/vial-lr/oled.c
@@ -36,6 +36,21 @@ void print_led_usb_state(void) {
     oled_write_P(PSTR(" N"), led_usb_state.num_lock);
 }
 
+void oled_white_space(void) {
+  oled_write_P(PSTR(" "), false);
+}
+
+// Adapted from keyboards/splitkb/kyria/keymaps/john-ezra/oled.c
+void render_mod_state(uint8_t modifiers) {
+  oled_write_P(PSTR("SHF"), (modifiers & MOD_MASK_SHIFT));
+  oled_white_space();
+  oled_write_P(PSTR("CTL"), (modifiers & MOD_MASK_CTRL));
+  oled_advance_page(false);
+  oled_write_P(PSTR("ALT"), (modifiers & MOD_MASK_ALT));
+  oled_white_space();
+  oled_write_P(PSTR("GUI"), (modifiers & MOD_MASK_GUI));
+}
+
 bool lr_oled_task_master(void) {
     oled_write_P(PSTR("Lyrael\n\n"), false);
     oled_write_P(IS_LAYER_ON_STATE(default_layer_state, _QWGAM) ? PSTR("Gam") : PSTR("Typ"), false);
@@ -46,8 +61,8 @@ bool lr_oled_task_master(void) {
     print_layer_status();
     next_line();
 
-//    oled_set_cursor(0, oled_max_lines()-3);
-//    print_sizing();
+    oled_set_cursor(0, oled_max_lines() - 6);
+    render_mod_state(get_mods()|get_oneshot_mods());
 
     return false;
 }
